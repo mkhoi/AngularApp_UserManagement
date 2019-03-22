8699750637560ae6ccba06d5cdb26a36d7cd581d
1212186 <m.khoi47@gmail.com>
2019-03-22 11:31:54 +0700

    update

--- src/users.ts
+++ src/users.ts
@@ -1,7 +1,19 @@
 import { Component } from "@angular/core";
+import { UserService } from "./userService";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "users",
     templateUrl: "src/users.html"
 })
-export class Users { }
+export class Users { 
+    
+    private users: Array<any>;
+    constructor(private router: Router, private userService: UserService) {
+        this.users = userService.getUser();
+    }
+
+    public onEditClicked(userId: string) {
+        this.router.navigate(["editUser", userId]);
+    }
+}
